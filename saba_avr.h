c2463044dcc1ef1aad48efd76edf02c3f9aa3ee5
jjpshb <joerg@5prim.de>
2017-10-14 10:49:32 +0200

    SFBITS is a collection of multiple bits

--- saba_avr.h
+++ saba_avr.h
@@ -3,7 +3,7 @@
  *
  * Saarbastler AVR C++ 11 Library
  *
- * Created: 24.06.2017 13:33:43
+ * Created: 24.06.2017 
  * Author: Joerg
  *
  * SFR, Port Templates
@@ -13,15 +13,36 @@
 #ifndef SABA_AVR_H_
 #define SABA_AVR_H_
 
+#include <saba_controller.h>
+
 namespace SABA
 {
 
   // Address of Special function Register
   typedef volatile uint16_t SFRA;
 
+  // Special Function Register 8 Bit or 16 Bit
+  /*template<typename TYPE,SFRA SFR_ADDRESS>
+  class _SFREG
+  {
+    public:
+    // read the SFR
+    TYPE operator () ()
+    {
+      return *(volatile TYPE *)SFR_ADDRESS;
+    }
+
+    // write the SFR
+    void operator =(TYPE value)
+    {
+      (*(volatile TYPE *)SFR_ADDRESS)= value;
+    }
+
+  };*/
+
   // Special Function Register
   template<SFRA SFR_ADDRESS>
-  class SFREG
+  class SFREG //: public _SFREG<uint8_t,SFR_ADDRESS>
   {
   public:
     // read the SFR
@@ -55,6 +76,24 @@ namespace SABA
     }
   };
 
+  // 16 Bit Special Function Register
+  template<SFRA SFR_ADDRESS>
+  class SFREG16 //: public _SFREG<uint16_t,SFR_ADDRESS>
+  {
+  public:
+    // read the SFR
+    uint16_t operator () ()
+    {
+      return *(volatile uint16_t *)SFR_ADDRESS;
+    }
+
+    // write the SFR
+    void operator =(uint16_t value)
+    {
+      (*(volatile uint16_t *)SFR_ADDRESS)= value;
+    }
+  };
+
   template<SFRA SFR_ADDRESS, uint8_t BIT_POS>
   class SFRBIT
   {
@@ -68,17 +107,62 @@ namespace SABA
     }
 
     // write the bit
-    void operator =(uint8_t value)
+    void operator =(bool value)
     {
       SFREG<SFR_ADDRESS> sfr;
 
       if( value)
         sfr |= _BV(BIT_POS);
       else
-        sfr &= ~_BV(BIT_POS);
+        sfr &= (uint8_t)~_BV(BIT_POS);
     }
   };
 
+  template<SFRA SFR_ADDRESS, uint8_t BIT_MASK>
+  class SFRBITS
+  {
+    public:
+    // read the bit
+    uint8_t operator () ()
+    {
+      SFREG<SFR_ADDRESS> sfr;
+
+      return sfr() & BIT_MASK;
+    }
+
+    // write the bits
+    void operator =(uint8_t value)
+    {
+      SFREG<SFR_ADDRESS> sfr;
+
+      sfr= (sfr() & ~BIT_MASK) | (value & BIT_MASK);
+    }
+
+    // or SFR
+    void operator |=(uint8_t value)
+    {
+      SFREG<SFR_ADDRESS> sfr;
+
+      sfr |= (value & BIT_MASK);
+    }
+
+    // and SFR
+    void operator &=(uint8_t value)
+    {
+      SFREG<SFR_ADDRESS> sfr;
+
+      sfr &= (value & BIT_MASK);
+    }
+
+    // exor SFR
+    void operator ^=(uint8_t value)
+    {
+      SFREG<SFR_ADDRESS> sfr;
+
+      sfr ^= (value & BIT_MASK);
+    }
+
+  };
 
   // AVR Port definition, the template parameter is the PIN address.
   template<SFRA PIN_ADDR>
@@ -120,7 +204,11 @@ namespace SABA
     {
       Port8<PIN_ADDR> port8;
 
-      port8.pin |= _BV(BIT_POS);
+#ifdef SUPPORTS_PIN_TOGGLE
+      port8.pin = _BV(BIT_POS);
+#else
+      port8.port ^= _BV(BIT_POS);
+#endif
     }
 
     void asOutput()
