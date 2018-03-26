fefbc6751e6675a903d6a22ab55366c75b63de43
jjpshb <joerg@5prim.de>
2018-03-26 20:36:53 +0200

    ATMega XXX4 CPU added

--- saba_controller.h
+++ saba_controller.h
@@ -16,7 +16,11 @@
 #if defined (__AVR_ATmega48__) | defined (__AVR_ATmega48A__) | defined (__AVR_ATmega48PA__) | defined (__AVR_ATmega48PB__) | defined (__AVR_ATmega48P__) \
   | defined (__AVR_ATmega88__) |  defined (__AVR_ATmega88A__) | defined (__AVR_ATmega88P__) |  defined (__AVR_ATmega88PA__) |  defined (__AVR_ATmega88PB__) \
   | defined (__AVR_ATmega168__) | defined (__AVR_ATmega168A__) | defined (__AVR_ATmega168P__) | defined (__AVR_ATmega168PA__) | defined (__AVR_ATmega168PB__) \
-  | defined (__AVR_ATmega328P__) | (defined __AVR_ATmega328__) | defined (__AVR_ATmega2560__)
+  | defined (__AVR_ATmega328P__) | defined (__AVR_ATmega328__) | defined (__AVR_ATmega2560__) \
+  | defined (__AVR_ATmega164P__) |defined (__AVR_ATmega164A__) | defined (__AVR_ATmega164PA__) \
+  | defined (__AVR_ATmega324P__) | defined (__AVR_ATmega324A__) | defined (__AVR_ATmega324PA__) \
+  | defined (__AVR_ATmega644__) | defined (__AVR_ATmega644A__) | defined (__AVR_ATmega644P__) | defined (__AVR_ATmega644PA__) \
+  | defined (__AVR_ATmega1284__) | defined (__AVR_ATmega1284P__)  \
 
 //! this controller supports a PORT toggle writing to the PIN register
 #define SUPPORTS_PIN_TOGGLE
